e80c8412da5711a3956e04e4ad6f7b96f02dba54
Camila <mkp.camila@gmail.com>
2022-01-04 14:22:25 +0100

    add error404

--- routerband/src/Pages/Error.jsx
+++ routerband/src/Pages/Error.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Error = () => {
     return (
         <div>
-            
+            <h1>Error 404</h1>
+            <p>Please still looking find what you're looking for</p>
         </div>
     )
 }
